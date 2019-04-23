44a708b2203cd471429ef6f1e5bdfdbbaafb881c
iRoot <root500@gmail.com>
2019-04-24 00:00:22 +0900

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app" class="app">
+    <div class="window">
+
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  components: {
+
+  }
+}
+</script>
+
+<style lang="scss">
+  @import "~normalize.css";
+
+  body {
+    margin: 0;
+  }
+  .app {
+
+  }
+  .window {
+    background-color: #f9f9f9;
+  }
+</style>
